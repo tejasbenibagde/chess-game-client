b48951c1282efb7befbafff6d35a5184804ffcc2
tejasbenibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-06-15 13:21:51 +0530

    Fixed routing, try-1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Route, createRoutesFromElements } from "react-router";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { Route } from "react-router";
 
 import { ThemeProvider } from "./ThemeProvider";
 
@@ -14,23 +13,19 @@ import PlayComputer from "./pages/PlayComputer";
 import TwoPlayer from "./pages/TwoPlayer";
 import Home from "./pages/Home";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<Home />} />
-      <Route path="/online-play" element={<OnlinePlay />} />
-      <Route path="/play-computer" element={<PlayComputer />} />
-      <Route path="/two-player" element={<TwoPlayer />} />
-    </Route>
-  )
-);
-
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   // <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <BrowserRouter>
+      <Routes>
+        <Route index={true} path="/" element={<Home />} />
+        <Route path="/online-play" element={<OnlinePlay />} />
+        <Route path="/play-computer" element={<PlayComputer />} />
+        <Route path="/two-player" element={<TwoPlayer />} />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
   // </React.StrictMode>
 );
